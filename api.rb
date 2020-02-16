de2df9b2d760acd0a49515b04b973be64583affd
Marino Gomez <marinogomez24@gmail.com>
2020-02-16 12:20:40 -0400

    API Completada
    API lista para su consumo, incluye CRUD y filtros de busqueda.

--- api.rb
+++ api.rb
@@ -1,5 +1,6 @@
 # Gems to use
 require 'sinatra'
+require 'sinatra/namespace'
 require 'mongoid'
 
 # DB Setup
@@ -11,24 +12,100 @@ class Location
   include Mongoid::Document
 
   field :name, type: String
-  has_one :position
+  field :position, type: Array
 
   validates :name, presence: true
-  validates :coordinates, presence: true
+  validates :position, presence: true
 
-  index(name: 'text')
+  index({name: 'text'})
+
+  scope :name, -> (name) { where(name: /^#{name}/) }
 end
 
-# Array of coordinates X and Y
-class Position
-  include Mongoid::Document
-  field :latitude, type: String
-  field :longitude, type: String
+# Serializer
+class LocationSerializer
+  def initialize(location)
+    @location = location
+  end
 
-  belongs_to :location
+  def as_json(*)
+    data = {
+      id:@location.id.to_s,
+      name:@location.name,
+      position:@location.position
+    }
+    data[:errors] = @location.errors if@location.errors.any?
+    data
+  end
 end
 
 # Endpoints
 get '/' do
-  'Welcome to Location Api.'
+  'Welcome to Locations List!'
+end
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message: 'Invalid JSON' }.to_json
+      end
+    end
+
+    def location
+      @location ||= Location.where(id: params[:id]).first
+    end
+
+    def halt_if_not_found!
+      halt(404, {message:'Location Not Found'}.to_json) unless location
+    end
+
+    def serialize(location)
+      LocationSerializer.new(location).to_json
+    end
+  end
+
+  get '/locations' do
+      locations = Location.all
+
+      [:name, :position].each do |filter|
+        locations = locations.send(filter, params[filter]) if params[filter]
+      end
+
+      locations.map { |location| LocationSerializer.new(location) }.to_json
+  end
+
+  get '/locations/:id' do |id|
+    halt_if_not_found!
+    serialize(location)
+  end
+
+  post '/locations' do
+    location = Location.new(json_params)
+    halt 422, serialize(location) unless location.save
+
+    response.headers['Location'] = "#{base_url}/api/v1/locations/#{location.id}"
+    status 201
+  end
+
+  patch '/locations/:id' do |id|
+    halt_if_not_found!
+    halt 422, serialize(location) unless location.update_attributes(json_params)
+    serialize(location)
+  end
+
+  delete '/locations/:id' do |id|
+    location.destroy if location
+    status 204
+  end
 end
